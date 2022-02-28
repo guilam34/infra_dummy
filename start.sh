1257d73a608412674a426a5456b508cb1725c9be
guilam34 <guillaumelam34@gmail.com>
2022-02-28 12:37:13 -0800

    Update start script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-app="docker.helloworld"
-docker build -t ${app} .
+app="flask-hello-world"
+docker build -t ${app}:latest .
 docker run -d -p 56733:80 \
   --name=${app} \
   -v $PWD:/app ${app}
